dd6da80454fa3d8d1dd103f97f3de39e849091ec
liv0ri <stefanialivroi2005@gmail.com>
2026-02-27 12:17:18 +0100

    updated config file

--- 5. Dementia Model/config.py
+++ 5. Dementia Model/config.py
@@ -1,5 +1,14 @@
-output_dir = "diagnosis"
-modalities = ["wav", "specto", "trans"]  # source folders
-original_classes = ["control", "dementia"]  # original folder names
-class_map = {"control": "cn", "dementia": "ad"}  # target folder names
-test_csv_name = "task1.csv"
+BATCH_SIZE = 64
+VAL_BATCH_SIZE = 32
+EPOCHS = 30
+GPUS = "0"
+LR = 3e-5
+NFINETUNE = 8
+N_FOLDS = 5
+
+OUTPUT_DIR = "diagnosis"
+MAIN_FOLDER = "5. Dementia Model"
+MODALITIES = ["wav", "specto", "trans"]
+ORIGINAL_CLASSES = ["control", "dementia"]
+CLASS_MAP = {"control": "cn", "dementia": "ad"}
+TEST_CSV_NAME = "task1.csv"
